b3169ddd5c7720eae00cec4844d4e5d8c3c7de97
Jeddy <jed.gong.nine@gmail.com>
2022-05-09 21:43:33 +0800

    feat(vite-vue3-admin): 新增vite-vue3-admin后台管理系统项目

---
+++ vite-vue3-admin/prettier.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    vueIndentScriptAndStyle: true,
+    singleQuote: true, // 单引号
+    quoteProps: 'as-needed',
+    bracketSpacing: true,
+    trailingComma: 'none',
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    arrowParens: 'always',
+    insertPragma: false,
+    requirePragma: false,
+    proseWrap: 'never',
+    htmlWhitespaceSensitivity: 'strict',
+    endOfLine: 'auto'
+};
---
+++ vite-vue3-admin/src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/login',
+        name: 'Login',
+        meta: {
+            title: '登录',
+            keepAlive: true,
+            requireAuth: false
+        },
+        component: () => import('@/views/login/login.vue')
+    },
+    {
+        path: '/',
+        name: 'Index',
+        meta: {
+            title: '首页',
+            keepAlive: true,
+            requireAuth: true
+        },
+        component: () => import('@/views/home/index.vue')
+    }
+];
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+export default router;
