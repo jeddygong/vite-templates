09d2da1556bed3b0d84f7b7d8f96dba14331f178
zayyo123 <764439554@qq.com>
2023-08-13 22:59:37 +0800

    添加Redux了

--- vite-react/src/App.tsx
+++ vite-react/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import routes from '@/router'
+import { Provider } from 'react-redux'
+import store from './store'
 
 function App() {
-  return <RouterProvider router={routes} />
+  return (
+    <Provider store={store}>
+      <RouterProvider router={routes} />
+    </Provider>
+  )
 }
 
 export default App
--- vite-react/src/views/home/Component/Home.tsx
+++ vite-react/src/views/home/Component/Home.tsx
@@ -1,12 +1,24 @@
 import React, { memo } from 'react'
 import type { ReactNode, FC } from 'react'
+import { useSelector } from 'react-redux'
 
 interface IProps {
   children?: ReactNode
 }
+interface RootState {
+  name: string
+  // 其他属性...
+}
 
 const Home: FC<IProps> = () => {
-  return <div>我是home页</div>
+  const data = useSelector((state: RootState) => state.name) // 替换为您的状态属性路径
+
+  return (
+    <div>
+      我是home页
+      <div>{data}</div>
+    </div>
+  )
 }
 
 export default memo(Home)
